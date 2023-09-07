4edb633657a012da6333f9f672f9a469428e50a8
LoanDaoudi <loanmateo.daoudi@gmail.com>
2023-09-07 19:52:43 +0200

    commit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Acceuil from './Component/Component/Acceuil.jsx';
+import Acceuil from './Component/Component/Acceuil';
 import MatrixBackground from './Component/Component/Matrix';
 import reportWebVitals from './reportWebVitals';
 
