d32ead2e4afe1df1a53dde599a8014152e36e9ef
LoanDaoudi <loanmateo.daoudi@gmail.com>
2023-09-08 15:16:33 +0200

    renouveau

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,14 @@
-import React from 'react';
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom'; 
 import Accueil from './Component/Component/Acceuil';
 import PageProjet from './Component/Component/Page-Projets';
-import Contact from './Component/Component/Contact';
 import './App.css';
-
 function App() {
   return (
     <BrowserRouter>
       <Routes> 
         <Route path="/" element={<Accueil />} />
         <Route path="/projets/:projectId" element={<PageProjet />} />
-        <Route path="/contact" element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Accueil from './Component/Component/Acceuil';
+import Acceuil from './Component/Component/Acceuil';
 import MatrixBackground from './Component/Component/Matrix';
+import App from './App'; 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <MatrixBackground />
-    <Accueil />
-    
+    <App >
+      <Acceuil/>
+    </App>
   </React.StrictMode>
 );
-
 
 reportWebVitals();
